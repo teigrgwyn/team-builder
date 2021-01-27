9ece4da5aec13111c0c2459a6468a77397903c98
Cory Young <teigrgwyn@gmail.com>
2021-01-26 19:43:42 -0600

    just need to render team member list now

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
-import Form from './components/Form';
+//import Form from './components/Form';
+import Styled from 'styled-components';
 import './App.css';
+
+const StyledInput = Styled.input`
+  text-align: center;
+`;
 
 const teamMembers = [
   // {
@@ -32,13 +37,43 @@ function App() {
 
   const onFormSubmit = event => {
     event.preventDefault();
-    console.log(formData);
+    teamMembers.push(formData);
+    console.log(teamMembers);
   }
 
   return (
+    // <Form formData={formData} onInputChange={onInputChange} onFormSubmit={onFormSubmit} />
     <div className="App">
-      <Form formData={formData} onInputChange={onInputChange} onFormSubmit={onFormSubmit} />
-      {teamMembers}
+      <div className='form-component'>
+        <h3>Add Team Member:</h3>
+        <form onSubmit={onFormSubmit}>
+          <StyledInput 
+            placeholder="name"
+            name='name'
+            onChange={onInputChange}
+          />
+          <StyledInput 
+            placeholder="email"
+            name='email'
+            onChange={onInputChange}
+          />
+          <StyledInput 
+            placeholder="role"
+            name='role'
+            onChange={onInputChange}
+          />
+          <input type='submit' />
+        </form>
+      </div>
+      {
+        teamMembers.map(user => (
+          <div className='user'>
+            <h4>Name: {user.name}</h4>
+            <h4>Email: {user.email}</h4>
+            <h4>Role: {user.role}</h4>
+          </div>
+        ))
+      }
     </div>
   );
 }
