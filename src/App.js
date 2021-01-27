4eea32cc14e3dcc3f8acd74ac108fc1f139b3410
Cory Young <teigrgwyn@gmail.com>
2021-01-26 19:18:37 -0600

    onInputChange test complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Form from './components/Form';
 import './App.css';
 
+const teamMembers = [
+  // {
+  //   name: 'Janice',
+  //   email: 'janice@gmail.com',
+  //   role: 'Director'
+  // },
+  // {
+  //   name: 'John',
+  //   email: 'john@gmail.com',
+  //   role: 'Team Lead'
+  // }
+]
+
 function App() {
+  const [formData, setFormData] = useState({ // form template expected
+    name: 'name',
+    email: 'email',
+    role: 'role'
+  });
+
+  const onInputChange = event => {
+    setFormData({ // format doesn't work for '.checked', does it?
+      ...formData, [event.target.name]: event.target.value,
+    })
+    console.log(formData);
+  }
+
+  const onFormSubmit = event => {
+    //event.preventDefault();
+    console.log("onFormSubmit");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form onInputChange={onInputChange} onFormSubmit={onFormSubmit} />
+      {teamMembers}
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Styled from 'styled-components';
+
+const StyledInput = Styled.input`
+  text-align: center;
+`;
+
+export default function Form(props) {
+  const { onInputChange, onFormSubmit } = props;
+
+  return (
+    <div className='form-component'>
+        <h3>Add Team Member</h3>
+        <form onSubmit={onFormSubmit}>
+          <StyledInput 
+            placeholder="name"
+            name='name'
+            onChange={onInputChange}
+          />
+          <StyledInput 
+            placeholder="email"
+            name='email'
+            onChange={onInputChange}
+          />
+          <StyledInput 
+            placeholder="role"
+            name='role'
+            onChange={onInputChange}
+          />
+        </form>
+      </div>
+  )
+}
